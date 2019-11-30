849aa40fc13dfae681ed671e4bbe01a12d0c7f11
GomezCanoDaniel <55300282+GomezCanoDaniel@users.noreply.github.com>
2019-11-29 19:50:14 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+    int n, opcion;
+    do
+    {
+        printf( "\n   1. Calcular el doble de un n%cmero entero.", 163 );
+        printf( "\n   2. Calcular la mitad de un n%cmero entero.", 163 );
+        printf( "\n   3. Calcular el cuadrado de un n%cmero entero.", 163 );
+        printf( "\n   4. Salir." );
+        do
+        {
